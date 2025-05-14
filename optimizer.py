12906b7e19868efe6b12a40a90cc3c42fbd13fe0
SeungMin Hong <2022113594@dgu.ac.kr>
2025-05-14 14:42:02 +0900

    opt, classifier

--- optimizer.py
+++ optimizer.py
@@ -45,20 +45,48 @@ class AdamW(Optimizer):
                 # Access hyperparameters from the `group` dictionary.
                 alpha = group["lr"]
 
-                '''
-                TODO: AdamW 구현을 완성하시오. 
-                    위의 state 딕셔너리를 사용하여 상태를 읽고 저장하시오.
-                    하이퍼파라미터는 `group` 딕셔너리에서 읽을 수 있다(생성자에 저장된 lr, betas, eps, weight_decay).
+                # Initialize state
+                if len(state) == 0:
+                    state["step"] = 0
+                    # First moment vector
+                    state["exp_avg"] = torch.zeros_like(p.data)
+                    # Second moment vector
+                    state["exp_avg_sq"] = torch.zeros_like(p.data)
 
-                        이 구현을 완성하기 위해서 해야할 일들:
-                          1. 그래디언트의 1차 모멘트(첫 번째 모멘트)와 2차 모멘트(두 번째 모멘트)를 업데이트.
-                          2. Bias correction을 적용(https://arxiv.org/abs/1412.6980 에 제공된 "efficient version" 사용; 프로젝트 설명의 pseudo-code에도 포함됨).
-                          3. 파라미터(p.data)를 업데이트.
-                          4. 그래디언트 기반의 메인 업데이트 후 weight decay 적용.
+                exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
+                beta1, beta2 = group["betas"]
+                eps = group["eps"]
+                weight_decay = group["weight_decay"]
+                correct_bias = group["correct_bias"]
 
-                        자세한 내용은 기본 프로젝트 안내문을 참조할 것.
-                '''
+                state["step"] += 1
+                step = state["step"]
+
+                # Update biased first moment estimate
+                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
+                # Update biased second raw moment estimate
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+
+                if correct_bias:
+                    bias_correction1 = 1 - beta1 ** step
+                    bias_correction2 = 1 - beta2 ** step
+                    corrected_exp_avg = exp_avg / bias_correction1
+                    corrected_exp_avg_sq = exp_avg_sq / bias_correction2
+
+                else:
+                    corrected_exp_avg = exp_avg
+                    corrected_exp_avg_sq = exq_avg_sq
+
+                denom = corrected_exp_avg_sq.sqrt().add_(eps)
+
+                # Parameter update
+                step_size = alpha
+                p.data.addcdiv_(corrected_exp_avg, denom, value=-step_size)
+
+                # Apply weight decay
+                if weight_decay != 0:
+                    p.data.add_(p.data, alpha=-alpha * weight_decay)
                 ### 완성시켜야 할 빈 코드 블록
-                raise NotImplementedError
+                # raise NotImplementedError
 
         return loss
